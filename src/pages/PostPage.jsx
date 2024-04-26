0136a5740c54aac09b3a30ca549e4fbb292fb43b
MauroTre <m.gutierrez7@hotmail.com>
2024-04-25 23:41:04 -0500

    comments section ready

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -1,7 +1,10 @@
-import { Avatar, Flex, Image, Text } from "@chakra-ui/react";
+import { Avatar, Box, Button, Divider, Flex, Image, Text } from "@chakra-ui/react";
 import { BsThreeDots } from "react-icons/bs";
+import { useState } from "react";
+import Comment from "../components/Comment";
 
 const PostPage = () => {
+    const { liked, setLiked } = useState(false);
   return (
     <>
       <Flex>
@@ -19,12 +22,47 @@ const PostPage = () => {
           </Flex>
         </Flex>
         <Flex gap={4} alignItems={"center"}>
-            <Text fontsize={"sm"} color={"gray.light"}>1d</Text>
-        <BsThreeDots />
+          <Text fontsize={"sm"} color={"gray.light"}>
+            1d
+          </Text>
+          <BsThreeDots />
         </Flex>
       </Flex>
 
       <Text my={3}>Let's talk about threads.</Text>
+
+      <Box
+        borderRadius={6}
+        overflow={"hidden"}
+        border={"1px solid"}
+        borderColor={"gray.light"}
+      >
+        <Image src="/post1.png" w={"full"} />
+      </Box>
+
+      <Flex gap={3} my={3}>
+        {/* <Actions liked={liked} setLiked={setLiked} /> */}
+      </Flex>
+
+      <Flex gap={3} alignItems={"center"}>
+        <Text color={"gray.light"} fontSize={"sm"}>238 replies</Text>
+        <Box w={0.5} h={0.5} borderRadius={"full"} bg={"gray.light"}></Box>
+        <Text color={"gray.light"} fontSize={"sm"}>{200 + (liked ? 1 : 0)} likes</Text>
+      </Flex>
+
+      <Divider my={4}/>
+
+      <Flex justifyContent={"space-between"} >
+        <Flex gap={2} alignItems={"center"}>
+            <Text fontSize={"2xl"}>👋</Text>
+            <Text color={"gray.light"}>Get the app to like, reply and post.</Text>
+        </Flex>
+        <Button>Get</Button>
+      </Flex>
+
+      <Divider my={4}/>
+
+      <Comment />
     </>
   );
 };
