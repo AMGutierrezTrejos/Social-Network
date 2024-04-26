f8f8bd93af94a6882c28894230f645c19d736019
MauroTre <m.gutierrez7@hotmail.com>
2024-04-25 23:12:55 -0500

    post building

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -1,13 +1,32 @@
-
-
+import { Avatar, Flex, Image, Text } from "@chakra-ui/react";
+import { BsThreeDots } from "react-icons/bs";
 
 const PostPage = () => {
-
   return (
     <>
+      <Flex>
+        <Flex w={"full"} alignItems={"center"} gap={3}>
+          <Avatar
+            src="/mauricio-avatar.jpg"
+            size={"md"}
+            name="Mauricio Gutierrez"
+          />
+          <Flex>
+            <Text fontsize={"sm"} fontWeight={"bold"}>
+              MauricioGutierrez
+            </Text>
+            <Image src="/verified.png" w="4" h={4} ml={4} />
+          </Flex>
+        </Flex>
+        <Flex gap={4} alignItems={"center"}>
+            <Text fontsize={"sm"} color={"gray.light"}>1d</Text>
+        <BsThreeDots />
+        </Flex>
+      </Flex>
 
+      <Text my={3}>Let's talk about threads.</Text>
     </>
-  )
-}
+  );
+};
 
-export default PostPage
+export default PostPage;
