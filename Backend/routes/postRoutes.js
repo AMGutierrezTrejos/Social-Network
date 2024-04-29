4cef9e53a0842c8d79234c5230cdace8df527550
MauroTre <m.gutierrez7@hotmail.com>
2024-04-29 11:56:38 -0500

    Reply Post Working

--- Backend/routes/postRoutes.js
+++ Backend/routes/postRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createPost, deletePost, getPost, likeUnlikePost } from "../controllers/postController.js";
+import { createPost, deletePost, getPost, likeUnlikePost, replyToPost } from "../controllers/postController.js";
 import protectRoute from "../middlewares/protectRoute.js";
 
 const router = express.Router();
@@ -9,6 +9,7 @@ router.get("/:id", getPost);
 
 router.post("/create", protectRoute ,createPost);
 router.post("/like/:id", protectRoute ,likeUnlikePost);
+router.post("/reply/:id", protectRoute ,replyToPost);
 
 router.delete("/:id", protectRoute,deletePost);
 
