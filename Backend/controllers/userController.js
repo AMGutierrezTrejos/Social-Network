dfd153260bb35a7c9f9c2d5f97b662e7f3ed5934
MauroTre <m.gutierrez7@hotmail.com>
2024-05-03 11:28:07 -0500

    Working

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -2,20 +2,35 @@ import User from "../models/userModel.js";
 import bycrypt from "bcryptjs";
 import generateTokenAndSetCookie from "../utils/helpers/generateTokenAndSetCookie.js";
 import { v2 as cloudinary } from "cloudinary";
+import mongoose from "mongoose";
 
 // Profile page
 const getUserProfile = async (req, res) => {
-  const { username } = req.params;
+  // We will fetch user profile either with username or userId
+  // query is either username or userId
+  const { query } = req.params;
 
   try {
-    const user = await User.findOne({ username })
-      .select("-password")
-      .select("-updatedAt");
+    let user;
+
+    // query is userId
+    if (mongoose.Types.ObjectId.isValid(query)) {
+      user = await User.findOne({ _id: query })
+        .select("-password")
+        .select("-updatedAt");
+    } else {
+      // query is username
+      user = await User.findOne({ username: query })
+        .select("-password")
+        .select("-updatedAt");
+    }
+
     if (!user) return res.status(404).json({ error: "User not found" });
 
     res.status(200).json(user);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+    console.log("Error in getUserProfile: ", err.message);
   }
 };
 
--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -1,13 +1,47 @@
-import { Button, Flex } from "@chakra-ui/react";
-import { Link } from "react-router-dom";
+import { Flex, Spinner } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import useShowToast from "../hooks/useShowToast";
+import Post from "../components/Post";
 
 const HomePage = () => {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const showToast = useShowToast();
+
+  useEffect(() => {
+    const getFeedPosts = async () => {
+      setLoading(true);
+      try {
+        const res = await fetch("/api/posts/feed");
+        const data = await res.json();
+        if (data.error) {
+          showToast("Error", data.error, "error");
+          return;
+        }
+        setPosts(data);
+      } catch (error) {
+        showToast("Error", error.message, "error");
+      } finally {
+        setLoading(false);
+      }
+    };
+    getFeedPosts();
+  }, [showToast]);
   return (
-    <Link to={"/AMGutierrezTrejos"}>
-      <Flex w={"full"} justifyContent={"center"}>
-        <Button mx={"auto"}>Visit Profile Page</Button>
-      </Flex>
-    </Link>
+    <>
+      {loading && (
+        <Flex justifyContent="center">
+          <Spinner size="xl" />
+        </Flex>
+      )}
+      {!loading && posts.length === 0 && (
+        <h1>Follow some users to see their posts</h1>
+      )}
+
+      {posts.map((post) => (
+        <Post key={post._id} post={post} postedBy={post.postedBy} />
+      ))}
+    </>
   );
 };
 
